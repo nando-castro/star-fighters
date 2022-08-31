17cbc54fae86df79ee4e5fcf9c7f73740aeb60bf
nando-castro <fernando.castro10.master@gmail.com>
2022-08-31 18:53:50 -0300

    chore: database config

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import cors from "cors";
+import express from "express";
+import dotenv from "dotenv";
+import router from "./routers";
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cors());
+/* app.use(router); */
+
+const { PORT } = process.env;
+
+app.listen(PORT, () => {
+  console.log(`server running on PORT ${PORT}`);
+});
